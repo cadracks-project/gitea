7727e7e9e8f5bc2c376649ff2617c034f43c6fdb
Gitea <gitea@fake.local>
2019-04-02 18:27:38 +0200

    cad3web files under GPLv3 license

--- routers/repo/cad2web_freecad_tests.py
+++ routers/repo/cad2web_freecad_tests.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# Copyright 2018-2019 Guillaume Florent
+
+# This source file is part of the present gitea fork (cad branch).
+#
+# The cad2web_*.py files is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# The cad2web_*.py files  are distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the cad2web_*.py files.  If not, see <https://www.gnu.org/licenses/>.
+
 r"""Tests for cad2web_freecad.py"""
 
 import pytest
